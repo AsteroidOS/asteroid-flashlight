0db72cf4d8dc2b3c1f126e6d9f7f45ed7fb867b7
MagneFire <IDaNLContact@gmail.com>
2021-11-18 20:07:50 +0100

    translations: Update translations.

---
+++ i18n/asteroid-flashlight.en_GB.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_GB">
+<context>
+    <name></name>
+    <message id="id-app-launcher-name">
+        <location filename="asteroid-flashlight.desktop.h" line="6"/>
+        <source>Flashlight</source>
+        <translation>Flashlight</translation>
+    </message>
+</context>
+</TS>
---
+++ i18n/asteroid-flashlight.nl_BE.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl_BE">
+<context>
+    <name></name>
+    <message id="id-app-launcher-name">
+        <location filename="asteroid-flashlight.desktop.h" line="6"/>
+        <source>Flashlight</source>
+        <translation>Zaklamp</translation>
+    </message>
+</context>
+</TS>
