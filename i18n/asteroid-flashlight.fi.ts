2943bf68a2fe24905ae965381caa5081f73b76b9
J. Lavoie <j.lavoie@net-c.ca>
2021-11-21 00:57:40 +0000

    Translated using Weblate (Finnish)
    Currently translated at 100.0% (1 of 1 strings)

--- i18n/asteroid-flashlight.fi.ts
+++ i18n/asteroid-flashlight.fi.ts
@@ -6,7 +6,7 @@
     <message id="id-app-launcher-name">
         <location filename="asteroid-flashlight.desktop.h" line="6"/>
         <source>Flashlight</source>
-        <translation type="unfinished"></translation>
+        <translation>Taskulamppu</translation>
     </message>
 </context>
 </TS>
