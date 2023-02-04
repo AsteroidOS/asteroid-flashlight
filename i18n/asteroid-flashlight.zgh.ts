450e4e15eaccb99923b1015c1525161a83f433e4
ⵣⵓⵀⵉⵔ ⴰⵎⴰⵣⵉⵖ ZOUHIR DEHBI <zouhirdehbi56@gmail.com>
2023-02-04 22:08:56 +0100

    Added translation using Weblate (Tamazight (Standard Moroccan))

---
+++ i18n/asteroid-flashlight.zgh.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zgh">
+<context>
+    <name></name>
+    <message id="id-app-launcher-name">
+        <location filename="asteroid-flashlight.desktop.h" line="6"/>
+        <source>Flashlight</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
