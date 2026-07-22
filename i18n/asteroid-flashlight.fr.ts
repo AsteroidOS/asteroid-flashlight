29d835e40384dda56e3c7120fc50d56d2bd0ac0d
moWerk <mo@mowerk.net>
2026-07-22 23:56:53 +0200

    Carry over beacon translations
    Add the new beacon string ids to every language catalog as unfinished
    entries, populated only where a translation already exists.

--- i18n/asteroid-flashlight.fr.ts
+++ i18n/asteroid-flashlight.fr.ts
@@ -8,5 +8,25 @@
         <source>Flashlight</source>
         <translation>Lampe de poche</translation>
     </message>
+    <message id="id-flashlight">
+        <location filename="../src/Flashlight.qml" line="22"/>
+        <source>Flashlight</source>
+        <translation>Lampe de poche</translation>
+    </message>
+    <message id="id-beacon">
+        <location filename="../src/Beacon.qml" line="73"/>
+        <source>Beacon</source>
+        <translation>Balise</translation>
+    </message>
+    <message id="id-beacon-pulse">
+        <location filename="../src/Beacon.qml" line="114"/>
+        <source>Pulse</source>
+        <translation>Pulsation</translation>
+    </message>
+    <message id="id-cat-emergency">
+        <location filename="../src/Beacon.qml" line="116"/>
+        <source>Emergency</source>
+        <translation>Urgence</translation>
+    </message>
 </context>
 </TS>
--- i18n/asteroid-flashlight.nl_NL.ts
+++ i18n/asteroid-flashlight.nl_NL.ts
@@ -8,5 +8,25 @@
         <source>Flashlight</source>
         <translation>Zaklamp</translation>
     </message>
+    <message id="id-flashlight">
+        <location filename="../src/Flashlight.qml" line="22"/>
+        <source>Flashlight</source>
+        <translation>Zaklamp</translation>
+    </message>
+    <message id="id-beacon">
+        <location filename="../src/Beacon.qml" line="73"/>
+        <source>Beacon</source>
+        <translation>Beacon</translation>
+    </message>
+    <message id="id-beacon-pulse">
+        <location filename="../src/Beacon.qml" line="114"/>
+        <source>Pulse</source>
+        <translation>Pulsatie</translation>
+    </message>
+    <message id="id-cat-emergency">
+        <location filename="../src/Beacon.qml" line="116"/>
+        <source>Emergency</source>
+        <translation>Noodgeval</translation>
+    </message>
 </context>
 </TS>
