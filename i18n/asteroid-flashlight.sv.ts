8a9892d31a069fb14fcc712fa5edfeb87b391f69
bittin1ddc447d824349b2 <bittin@reimu.nl>
2026-09-06 14:29:56 +0200

    Translated using Weblate (Swedish)
    Currently translated at 100.0% (5 of 5 strings)

--- i18n/asteroid-flashlight.sv.ts
+++ i18n/asteroid-flashlight.sv.ts
@@ -11,17 +11,17 @@
     <message id="id-beacon">
         <location filename="../src/Beacon.qml" line="73"/>
         <source>Beacon</source>
-        <translation type="unfinished"></translation>
+        <translation>Fyr</translation>
     </message>
     <message id="id-beacon-pulse">
         <location filename="../src/Beacon.qml" line="114"/>
         <source>Pulse</source>
-        <translation type="unfinished"></translation>
+        <translation>Puls</translation>
     </message>
     <message id="id-cat-emergency">
         <location filename="../src/Beacon.qml" line="116"/>
         <source>Emergency</source>
-        <translation type="unfinished"></translation>
+        <translation>Nödsituation</translation>
     </message>
     <message id="id-flashlight">
         <location filename="../src/Flashlight.qml" line="22"/>
