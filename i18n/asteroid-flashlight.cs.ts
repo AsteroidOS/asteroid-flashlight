e5e4b72471e2eeb784a9a0224906e9dacf3850e5
Pavel Borecki <pavel.borecki@gmail.com>
2026-09-09 10:57:47 +0200

    Translated using Weblate (Czech)
    Currently translated at 100.0% (5 of 5 strings)

--- i18n/asteroid-flashlight.cs.ts
+++ i18n/asteroid-flashlight.cs.ts
@@ -11,17 +11,17 @@
     <message id="id-beacon">
         <location filename="../src/Beacon.qml" line="73"/>
         <source>Beacon</source>
-        <translation type="unfinished"></translation>
+        <translation>Bóje</translation>
     </message>
     <message id="id-beacon-pulse">
         <location filename="../src/Beacon.qml" line="114"/>
         <source>Pulse</source>
-        <translation type="unfinished"></translation>
+        <translation>Pulzující</translation>
     </message>
     <message id="id-cat-emergency">
         <location filename="../src/Beacon.qml" line="116"/>
         <source>Emergency</source>
-        <translation type="unfinished"></translation>
+        <translation>Nouzové</translation>
     </message>
     <message id="id-flashlight">
         <location filename="../src/Flashlight.qml" line="22"/>
