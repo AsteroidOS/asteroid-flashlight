e9008d056405d2f3693fa1dee6fd5b5ed27587b8
ManuelBoe <manuelboe@linuxmail.org>
2026-09-13 19:22:34 +0200

    Translated using Weblate (Spanish (Argentina))
    Currently translated at 100.0% (5 of 5 strings)

--- i18n/asteroid-flashlight.es_AR.ts
+++ i18n/asteroid-flashlight.es_AR.ts
@@ -11,17 +11,17 @@
     <message id="id-beacon">
         <location filename="../src/Beacon.qml" line="73"/>
         <source>Beacon</source>
-        <translation type="unfinished"></translation>
+        <translation>Baliza</translation>
     </message>
     <message id="id-beacon-pulse">
         <location filename="../src/Beacon.qml" line="114"/>
         <source>Pulse</source>
-        <translation type="unfinished"></translation>
+        <translation>Pulsar</translation>
     </message>
     <message id="id-cat-emergency">
         <location filename="../src/Beacon.qml" line="116"/>
         <source>Emergency</source>
-        <translation type="unfinished"></translation>
+        <translation>Emergencia</translation>
     </message>
     <message id="id-flashlight">
         <location filename="../src/Flashlight.qml" line="22"/>
